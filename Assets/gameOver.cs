8c4099daada8d10b572a9a509ea33a7f922d8c61
Xinyu Bai <53417086+chelseabai@users.noreply.github.com>
2021-07-21 22:20:07 +0100

    Add files via upload

---
+++ Assets/gameOver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameOver : MonoBehaviour
+{
+	public Text gameOverText;
+
+
+    // Start is called before the first frame update
+    public void setUp()
+    {
+    	gameOverText.GetComponent<Text>().enabled = false;
+    }
+
+    public void displayText()
+    {
+    	gameOverText.GetComponent<Text>().enabled = true;
+    }
+
+    
+}
