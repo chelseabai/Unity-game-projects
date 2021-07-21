ae895d009a48c678b0794dc4dc6ee83da3e17970
Xinyu Bai <53417086+chelseabai@users.noreply.github.com>
2021-07-21 22:35:00 +0100

    Add files via upload

---
+++ Tetris-game/spawnTetromino.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnTetromino : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Tetrominoes;
+    void Start()
+    {
+        NewTetromino();
+    }
+
+
+    public void NewTetromino()
+    {
+    	Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+    }
+}
